3dc1168301a4d85a421b3b27bff5299c8160f532
Drew D <drewdowdy109@gmail.com>
2024-07-03 23:26:23 -0400

    new 6kyu file, and added link to previous files

--- 7kyu/Sum_a_list_but_ignore_any_duplicates.rb
+++ 7kyu/Sum_a_list_but_ignore_any_duplicates.rb
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/5993fb6c4f5d9f770c0000f2
+
 # Please write a function that sums a list, but ignores any duplicated items in the list.
 
 # For instance, for the list [3, 4, 3, 6] the function should return 10,
